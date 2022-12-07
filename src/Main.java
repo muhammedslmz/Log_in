a921fc3cc7f489a5e1f5f9f72f72e88079262279
Muhammed Solmaz <muhammed.slmz@gmail.com>
2022-12-07 16:15:16 +0300

    thanksalot is the password. Don't forget :b

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String username, password;
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter the username: ");
+        username=input.nextLine();
+
+        System.out.print("Enter the password: ");
+        password=input.nextLine();
+
+        if(username.equals("muhammedslmz")&&password.equals("thanksalot"))
+        {
+            System.out.println("Login successful.");
+        }
+        else
+        {
+            System.out.println("Login failed");
+        }
+    }
+}
